4423b29ae184ea1dc285f18ab7c78d7d07b4b356
GuoLiBin6 <glbin@163.com>
2021-10-04 22:20:07 +0800

    feat: add token check
    添加token验证测试内容

---
+++ app/controller/token.js
@@ -0,0 +1,18 @@
+const Controller = require('egg').Controller;
+
+class TokenController extends Controller {
+    async index(args) {
+        const { originalUrl = '' } = args
+        const queryStr = originalUrl.split('?')[1] || ''
+        const queryList = queryStr.split('&');
+        const queryMap = {}
+        queryList.map(item=>{
+            const itemList = item.split('=')
+            queryMap[itemList[0]] = itemList[1] || '';
+        })
+        console.log(queryMap)
+        this.ctx.body = queryMap['echostr'] || '';
+    }
+}
+
+module.exports = TokenController;
--- app/router.js
+++ app/router.js
@@ -1,5 +1,6 @@
 module.exports = app => {
     const { router, controller } = app;
+    // router.get('/', controller.token.index);
     router.get('/', controller.home.index);
     router.get('/news', controller.news.list);
 };
