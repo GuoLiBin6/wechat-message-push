de8a7633da30d26e4d5d60732e091106db7f7fb6
GuoLiBin6 <glbin@163.com>
2022-01-04 21:48:36 +0800

    feat: add templateData to wechat

--- app/router.js
+++ app/router.js
@@ -3,4 +3,5 @@ module.exports = app => {
     // router.get('/', controller.token.index);
     router.get('/', controller.home.index);
     router.get('/news', controller.news.list);
+    router.get('/send', controller.home.send);
 };
